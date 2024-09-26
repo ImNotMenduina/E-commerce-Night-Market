9560f88405d39a920bad8ea49b4ecb540d0eddaf
ImNotMenduina <mynameis_menduina@outlook.com>
2024-09-26 02:11:49 -0300

    add: alpine modal to present items

--- app/controllers/weapons_controller.ts
+++ app/controllers/weapons_controller.ts
@@ -1,24 +1,30 @@
 import type { HttpContext } from '@adonisjs/core/http'
 
 export default class WeaponsController {
+  async get_weapons({ view }: HttpContext) {
+    const response = await fetch('https://valorant-api.com/v1/weapons')
+    const data = await response.json()
+    return view.render('pages/weapons/category', { value: data })
+  }
 
-    async get_weapons( {view} : HttpContext) {
-        const response = await fetch("https://valorant-api.com/v1/weapons")
-        const data = await response.json()
-        return view.render('pages/weapons/category', {value : data})
-    }
+  async get_available_skins({ params, view }: HttpContext) {
+    const response = await fetch('https://valorant-api.com/v1/weapons/skins')
+    const { data } = await response.json()
 
-    async get_available_skins( {params, view} : HttpContext ) {
-        const response = await fetch("https://valorant-api.com/v1/weapons/skins")
-        const {data} = await response.json()
-        const skins = []
+    // pre-processing skins //
+    const skins = data.filter(
+      (item) =>
+        item.displayName.includes(params.category) &&
+        item.displayIcon != null &&
+        !item.displayName.includes('Standard')
+    )
+    // return all weapon skins //
+    return view.render('pages/weapons/skins', { value: skins, name: params.category })
+  }
 
-        for(const category_skin of data) {
-            if (category_skin.displayName.includes(params.category)) {
-                skins.push(category_skin)
-            }
-        }
-        // return all weapon skins
-        return view.render('pages/weapons/skins', {value : skins, name : params.category})
-    }
+  async get_skin({ params, view }: HttpContext) {
+    const response = await fetch(`https://valorant-api.com/v1/weapons/skins/${params.skinUuid}`)
+    const { data } = await response.json()
+    return view.render('pages/weapons/skin', { name: params.category, skin: data })
+  }
 }
