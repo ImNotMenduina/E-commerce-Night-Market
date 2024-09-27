97bab0625c499a824c9e171c12adf50d0e79d619
ImNotMenduina <mynameis_menduina@outlook.com>
2024-09-27 13:52:36 -0300

    fix: fix router group/prefix weapon acts

--- start/routes.ts
+++ start/routes.ts
@@ -10,12 +10,16 @@
 import router from '@adonisjs/core/services/router'
 
 const WeaponsController = () => import('#controllers/weapons_controller')
-const BundlesController = () => import('#controllers/bundles_controller')
+//const BundlesController = () => import('#controllers/bundles_controller')
 
 router.on('/').render('pages/home')
 
-router.get('/weapons', [WeaponsController, 'get_weapons'])
-router.get('/weapons/:category', [WeaponsController, 'get_available_skins']).as('category')
-router.get('/weapons/:category/skin/:skinUuid', [WeaponsController, 'get_skin']).as('skin')
+router
+  .group(() => {
+    router.get('/', [WeaponsController, 'get_weapons']).as('home')
+    router.get('/:category', [WeaponsController, 'get_available_skins']).as('category')
+    router.get('/:category/skin/:skinUuid', [WeaponsController, 'get_skin']).as('skin')
+  })
+  .prefix('/weapon')
 
-router.get('/bundles', [BundlesController, 'get_available_bundles']).as('bundles')
+//router.get('/bundles', [BundlesController, 'get_available_bundles']).as('bundles')
