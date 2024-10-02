0961338b5444606ca63462ee50324c4ec45bec58
ImNotMenduina <mynameis_menduina@outlook.com>
2024-10-02 01:39:01 -0300

    add: add skin models and migration

--- app/models/skin.ts
+++ app/models/skin.ts
@@ -6,6 +6,9 @@ export default class Skin extends BaseModel {
   declare id: number
 
   @column()
+  declare skinUuid: string
+
+  @column()
   declare displayName: string
 
   @column()
--- database/migrations/1727841382432_create_skins_table.ts
+++ database/migrations/1727841382432_create_skins_table.ts
@@ -6,6 +6,7 @@ export default class extends BaseSchema {
   async up() {
     this.schema.createTable(this.tableName, (table) => {
       table.increments('id')
+      table.string('skin_uuid')
       table.string('display_name')
       table.string('display_icon')
       table.string('theme_uuid')
