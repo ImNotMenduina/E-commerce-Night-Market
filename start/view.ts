401802d02716b7fd9cf5c8b5fb6c7d65fa6b3b8d
ImNotMenduina <mynameis_menduina@outlook.com>
2024-10-25 22:46:05 -0300

    add: add user card and fix signin page

--- start/view.ts
+++ start/view.ts
@@ -2,11 +2,13 @@ import edge from 'edge.js'
 import env from '#start/env'
 import { edgeIconify, addCollection } from 'edge-iconify'
 import { icons as heroIcons } from '@iconify-json/heroicons'
+import { icons as heroIconSolid } from '@iconify-json/heroicons-solid'
 /**
  * Register a plugin
  */
 edge.use(edgeIconify)
 addCollection(heroIcons)
+addCollection(heroIconSolid)
 /**
  * Define a global property
  */
