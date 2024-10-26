6e997914a69668a649d11bfc5cebe19f76bdbd5b
ImNotMenduina <mynameis_menduina@outlook.com>
2024-10-26 03:03:33 -0300

    fix: fix favorite btn and skin page

--- start/view.ts
+++ start/view.ts
@@ -3,12 +3,14 @@ import env from '#start/env'
 import { edgeIconify, addCollection } from 'edge-iconify'
 import { icons as heroIcons } from '@iconify-json/heroicons'
 import { icons as heroIconSolid } from '@iconify-json/heroicons-solid'
+import { icons as heroIconOutline } from '@iconify-json/heroicons-outline'
 /**
  * Register a plugin
  */
 edge.use(edgeIconify)
 addCollection(heroIcons)
 addCollection(heroIconSolid)
+addCollection(heroIconOutline)
 /**
  * Define a global property
  */
